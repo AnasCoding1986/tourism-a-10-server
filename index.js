1ef2ec64a50c387603f31cd20bf58655a3e406b8
AnasCoding1986 <anassust.scw@gmail.com>
2024-04-28 04:35:04 +0600

    view details done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,12 +30,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const spotCollection = client.db('spotDB').collection('spot');
+
+    app.get('/spot', async(req,res) => {
+        const cursor = spotCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/spot/:id', async(req,res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const spot = await spotCollection.findOne(query);
+        res.send(spot)
+    })
+
+    app.post('/spot', async(req,res) => {
+        const newSpot = req.body;
+        console.log(newSpot);
+        const result = await spotCollection.insertOne(newSpot);
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
