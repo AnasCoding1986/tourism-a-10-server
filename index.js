9aba48765eb8ec00a5c845c4fcd914909a37cb9f
AnasCoding1986 <anassust.scw@gmail.com>
2024-04-30 04:25:26 +0600

    update isssues solved

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ async function run() {
 
     app.put('/spot/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = { _id = new ObjectId(id) };
+      const filter = { _id : new ObjectId(id) };
       const options = { upsert: true };
       const updatedSpot = req.body;
       const spot = {
